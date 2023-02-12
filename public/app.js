41719c8b2cf79d10c0f74dd42b773753059965c3
Vicky Kumar <vicky4830.be21@chitkara.edu.in>
2023-02-12 16:36:38 +0530

    Code Files

---
+++ public/app.js
@@ -0,0 +1,20 @@
+$('#navbar').load('navbar.html');
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function (device) {
+  $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+  );
+});
+
+$('#add-device').on('click', function () {
+  const user = $('#user').val();
+  const name = $('#name').val();
+  devices.push({ user: user, name: name });
+  localStorage.setItem('devices', JSON.stringify(devices));
+  location.href = '/';
+});
