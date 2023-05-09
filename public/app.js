e35de253ed992905062092570b08a34c7e30b8c2
Vicky Kumar <vicky4830.be21@chitkara.edu.in>
2023-05-09 19:46:59 +0530

    files

--- public/app.js
+++ public/app.js
@@ -1,20 +1,252 @@
-$('#navbar').load('navbar.html');
+const API_URL = 'http://localhost:5000/api';
+const Sensor_URL = 'http://localhost:5003/sensorvalues';
 
-const devices = JSON.parse(localStorage.getItem('devices')) || [];
-
-devices.forEach(function (device) {
-  $('#devices tbody').append(`
+$.get(`${API_URL}/lightdevices`)
+  .then(response => {
+    response.forEach(device => {
+      $('#lightdevices tbody').append(`
       <tr>
-        <td>${device.user}</td>
-        <td>${device.name}</td>
+        <td>${device.room}</td>
+        <td>${device.devicename}</td>
+        <td>${device.state}</td>
+        <td style="background: ${device.color}"></td>
       </tr>`
-  );
+      );
+    });
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+
+$.get(`${API_URL}/securitydevices`)
+  .then(response => {
+    response.forEach(device => {
+      $('#securitydevices tbody').append(`
+      <tr>
+        <td>${device.room}</td>
+        <td>${device.devicename}</td>
+        <td>${device.state}</td>
+      </tr>`
+      );
+    });
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+
+$('#light-device').on('click', function () {
+  const room = $('#user').val();
+  const devicename = $('#name').val();
+  const state = $('#state').val();
+  const color = $('#color').val();
+
+  const body = {
+    room,
+    devicename,
+    state,
+    color
+  }
+
+  $.post(`${API_URL}/lightdevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
 });
 
-$('#add-device').on('click', function () {
-  const user = $('#user').val();
-  const name = $('#name').val();
-  devices.push({ user: user, name: name });
-  localStorage.setItem('devices', JSON.stringify(devices));
-  location.href = '/';
+$('#remove-lightdevice').on('click', function () {
+  const room = $('#user').val();
+  const devicename = $('#name').val();
+  const state = 'ON';
+
+  const body = {
+    room,
+    devicename,
+    state
+  }
+
+  $.post(`${API_URL}/removelightdevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
 });
+
+$('#remove-securitydevice').on('click', function () {
+  const room = $('#user').val();
+  const devicename = $('#name').val();
+
+  const body = {
+    room,
+    devicename
+  }
+
+  $.post(`${API_URL}/removesecuritydevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
+});
+
+$('#security-device').on('click', function () {
+  const room = $('#user').val();
+  const devicename = $('#device').val();
+  const state = $('#state').val();
+
+  const body = {
+    room,
+    devicename,
+    state
+  }
+
+  $.post(`${API_URL}/securitydevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
+});
+
+$('#changelightdevicestate').on('click', function () {
+  const room = $('#rooms').val();
+  const state = $('#state').val();
+  const color = $('#customColor').val();
+
+  const body = {
+    room,
+    state,
+    color
+  };
+
+  $.post(`${API_URL}/updatedevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
+});
+
+$('#changesecuritydevicestate').on('click', function () {
+  const room = $('#rooms').val();
+  const devicename = 'LED';
+  const state = $('#state').val();
+
+  const body = {
+    room,
+    devicename,
+    state
+  };
+
+  $.post(`${API_URL}/updatesecuritydevices`, body)
+    .then(response => {
+      location.href = 'device-list.html';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
+});
+
+let ultrasensordata = [];
+let motionsensordata = [];
+let motionsensorvaluedata = [];
+let timedata = [];
+let lastultraval = [];
+let lastmotionval = [];
+let lasttimeval = [];
+
+$.get(`${Sensor_URL}/sensor-values`)
+  .then(response => {
+    response.forEach(device => {
+      ultrasensordata.push(device.ultradata);
+    });
+
+    response.forEach(device => {
+      motionsensorvaluedata.push(device.motiondata);
+    });
+
+    response.forEach(device => {
+      if (device.motiondata == 1) {
+        motionsensordata.push('Motion detected');
+      }
+      if (device.motiondata == 0) {
+        motionsensordata.push('No Motion detected');
+      }
+    });
+
+    response.forEach(device => {
+      timedata.push(device.date);
+    });
+
+    lastultraval = ultrasensordata.slice(-150);
+    lastmotionval = motionsensordata.slice(-150);
+    lasttimeval = timedata.slice(-150);
+
+    for (let i = 0; i < lasttimeval.length; i++) {
+      const newRow = $('<tr>');
+      newRow.append(`<td>${lastultraval[i]}</td>`);
+      newRow.append(`<td>${lastmotionval[i]}</td>`);
+      newRow.append(`<td>${lasttimeval[i]}</td>`);
+      $('#sensorvalues tbody').append(newRow);
+    }
+
+    let timechart = timedata.map(function (time) {
+      let timeParts = time.split(":");
+      let hours = parseInt(timeParts[0]);
+      let minutes = parseInt(timeParts[1]);
+      let seconds = parseInt(timeParts[2]);
+
+      return Math.floor(minutes + seconds / 3600);
+    });
+
+    const numericYArray = ultrasensordata.map(val => parseFloat(val));
+
+    Highcharts.chart('my-plot', {
+      title: {
+        text: 'Graph b/w tds value and time'
+      },
+      xAxis: {
+        title: {
+          text: 'Time (in hours)'
+        },
+        categories: timechart
+      },
+      yAxis: {
+        title: {
+          text: 'tds value'
+        },
+      },
+      series: [{
+        data: numericYArray
+      }]
+    });
+
+    const numericYArray1 = motionsensorvaluedata.map(val => parseFloat(val));
+
+    Highcharts.chart('plot', {
+      title: {
+        text: 'Graph b/w loadcell value and time'
+      },
+      xAxis: {
+        title: {
+          text: 'Time (in hours)'
+        },
+        categories: timechart
+      },
+      yAxis: {
+        title: {
+          text: 'Load cell value'
+        },
+      },
+      series: [{
+        data: numericYArray1
+      }]
+    });
+  })
--- server.js
+++ server.js
@@ -1,29 +1,152 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const swaggerjsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const port = 3000;
 const base = `${__dirname}/public`;
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const GOOGLE_CLIENT_ID = '134720855276-7b7gooaenpdkni82v98rqv3i5l11ii78.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/google/callback",
+    passReqToCallback: true,
+},
+    function (request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Active Sense",
+            version: "0.1.0",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000.com",
+            },
+        ],
+    },
+    apis: ["./public/*.js"],
+};
+
+const specs = swaggerjsdoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/auth/google');
+}
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/google/callback', passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure'
+}));
+
+app.get('/protected', isLoggedIn, (req, res) => {
     res.sendFile(`${base}/device-list.html`);
 });
 
-app.get('/register-device', (req, res) => {
+app.get('/', function (req, res) {
+    res.sendFile(`${base}/main.html`);
+});
+
+app.get('/protected/air-conditioning', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/air-conditioning.html`);
+});
+
+app.get('/protected/feature', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/feature.html`);
+});
+
+app.get('/protected/lightdevice', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/lightdevice.html`);
+});
+
+app.get('/protected/lightning', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/lightning.html`);
+});
+
+app.get('/protected/device-list', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/device-list.html`);
+});
+
+app.get('/protected/register-device', isLoggedIn, (req, res) => {
     res.sendFile(`${base}/register-device.html`);
 });
 
-app.get('/iot-applications', (req, res) => {
-    res.sendFile(`${base}/iot-applications.html`);
+app.get('/protected/remove-device', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/remove-device.html`);
 });
 
-app.get('/Contact', (req, res) => {
-    res.sendFile(`${base}/Contact.html`);
+app.get('/protected/removelight', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/removelight.html`);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(`${base}/404.html`);
+app.get('/protected/removesecurity', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/removesecurity.html`);
+});
+
+app.get('/protected/security', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/security.html`);
+});
+
+app.get('/protected/securitydevice', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/securitydevice.html`);
+});
+
+app.get('/protected/send-command', isLoggedIn, (req, res) => {
+    res.sendFile(`${base}/send-command.html`);
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.send('Goodbye!');
+    req.session.destroy();
+});
+
+app.get('/auth/google/failure', (req, res) => {
+    res.send('Failed to authenticate..');
 });
 
 app.listen(port, () => {
