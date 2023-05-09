b39a8eda0254f0b509e66eaa65e8b7508c83a24f
Vicky Kumar <vicky4830.be21@chitkara.edu.in>
2023-05-10 01:07:19 +0530

    Fifth Update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000.com",
+                url: "https://individualweb.onrender.com",
             },
         ],
     },
